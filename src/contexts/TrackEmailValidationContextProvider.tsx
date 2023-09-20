1760d585194977d32e29b366c70e2306831b8d58
Roz <rosalyn.bonilla@podium.com>
2023-09-20 22:19:37 +0100

    add context for tracking email requests

---
+++ src/contexts/TrackEmailValidationContextProvider.tsx
@@ -0,0 +1,59 @@
+import React, {
+  createContext,
+  type PropsWithChildren,
+  useContext,
+  useState,
+} from 'react';
+
+const VALIDATION_REQUESTS_IDENTIFIER = 'validationRequests';
+
+export type EmailValidationRequestType = {
+  email: string;
+  result: string;
+};
+export type EmailValidationRequestContextType = {
+  addRequest?: (_emailRequest: EmailValidationRequestType) => void;
+  emailValidationRequests: EmailValidationRequestType[] | [];
+};
+
+export const TrackEmailValidationContext =
+  createContext<EmailValidationRequestContextType>({
+    addRequest: (_emailRequest: EmailValidationRequestType) => {},
+    emailValidationRequests: [],
+  });
+export function useTrackEmailValidationContext() {
+  return useContext(TrackEmailValidationContext);
+}
+
+function TrackEmailValidationContextProvider({ children }: PropsWithChildren) {
+  const [emailValidationRequests, setEmailValidationRequests] = useState<
+    EmailValidationRequestType[]
+  >(() => {
+    const storedCount = localStorage.getItem(VALIDATION_REQUESTS_IDENTIFIER);
+    return storedCount ? JSON.parse(storedCount) : [];
+  });
+
+  function addRequest(emailRequest: EmailValidationRequestType) {
+    const updatedValidationRequests = [
+      ...emailValidationRequests,
+      emailRequest,
+    ];
+
+    setEmailValidationRequests(updatedValidationRequests);
+
+    localStorage.setItem(
+      VALIDATION_REQUESTS_IDENTIFIER,
+      JSON.stringify(updatedValidationRequests),
+    );
+  }
+
+  return (
+    <TrackEmailValidationContext.Provider
+      value={{ addRequest, emailValidationRequests }}
+    >
+      {children}
+    </TrackEmailValidationContext.Provider>
+  );
+}
+
+export default TrackEmailValidationContextProvider;
